06d8b7616731f40026b018a22f9e29102e5ff8ce
Paulo eduardo Loyola ramires <ployola-@c1r20s9.42lisboa.com>
2021-10-20 13:19:20 +0100

    commit 4

--- Libft/memmove.c
+++ Libft/memmove.c
@@ -9,5 +9,22 @@
 /*   Updated: 2021/10/19 12:34:51 by ployola-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void	*memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	unsigned char 	*tmp1;
+	unsigned char	*tmp2;
 
+	tmp1 = (unsigned char *)dst;
+	tmp2 = (unsigned char *)src;
+	i = 0;
+	if (tmp2 < tmp1)
+		while (++i <= len)
+			tmp1[len - i] = tmp2[len - i];
+	else
+		while (len-- > 0)
+			*(tmp1++) = *(tmp2++);
+	return (dst);
+}
--- Libft/memset.c
+++ Libft/memset.c
@@ -9,5 +9,16 @@
 /*   Updated: 2021/10/19 12:35:01 by ployola-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char*)b;
+	while (len-- > 0)
+		*(ptr++) = (unsigned char)c;
+	return (b);
+}
 
 
