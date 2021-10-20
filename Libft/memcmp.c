20f4f584e7c1923c7a93ebe4e1ecc6a2729222b7
Paulo eduardo Loyola ramires <ployola-@c1r20s9.42lisboa.com>
2021-10-20 12:29:59 +0100

    added mem cmp and cpy

--- Libft/memcmp.c
+++ Libft/memcmp.c
@@ -9,5 +9,22 @@
 /*   Updated: 2021/10/19 12:30:22 by ployola-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned const char	*tmp1;
+	unsigned const char	*tmp2;
 
+	tmp1 = (unsigned const char *)s1;
+	tmp2 = (unsigned const char *)s2;
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	while (n > 0)
+	{
+		if (*tmp1++ != *tmp2++)
+			return (*--tmp1 - *--tmp2);
+		n--;
+	}
+	return (0);
+}
--- Libft/memcpy.c
+++ Libft/memcpy.c
@@ -9,5 +9,16 @@
 /*   Updated: 2021/10/19 12:34:41 by ployola-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	unsigned char	*tmp1;
+	unsigned char	*tmp2;
 
+	tmp1 = (unsigned char *)dst;
+	tmp2 = (unsigned char *)src;
+	while (n-- > 0)
+		*(tmp1++) = *(tmp2++);
+	return (dst);
+}
